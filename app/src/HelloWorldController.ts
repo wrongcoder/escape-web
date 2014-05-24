1387bf4ecb4bb2e9c93a1e64fa6d6f22e15f520f
wrongcoder <git@wrongcoder.com>
2014-05-23 20:58:06 -0700

    Add type information

--- app/src/HelloWorldController.ts
+++ app/src/HelloWorldController.ts
@@ -1,11 +1,17 @@
 /// <reference path="../reference.ts" />
 
 module Application {
+
+	export interface HelloWorldControllerScope {
+		number: number;
+	}
+
 	export class HelloWorldController {
 		//noinspection JSUnusedGlobalSymbols
 		static $inject = [ "$scope" ];
-		constructor($scope) {
+		constructor($scope: HelloWorldControllerScope) {
 			$scope.number = 3;
 		}
 	}
+
 }
