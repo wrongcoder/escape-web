83b0eb98a70a5fee284e0055d5a8ef02b4bb62c6
wrongcoder <git@wrongcoder.com>
2014-06-22 10:58:19 -0700

    Set return type

--- app/src/HelloWorldController.ts
+++ app/src/HelloWorldController.ts
@@ -17,11 +17,11 @@ module Application {
 			$scope.c = this;
 		}
 
-		increment() {
+		increment(): void {
 			this.number += 1;
 		}
 
-		decrement() {
+		decrement(): void {
 			this.number -= 1;
 		}
 	}
